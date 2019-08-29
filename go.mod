62a2a61d310aed97250ab53777d296ef0f8c2755
fbigg@afx.gmail.com <fbigg>
2019-08-29 22:04:52 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/a97077088/threadpool
+
+go 1.12
---
+++ threadpool.go
@@ -0,0 +1,53 @@
+package threadpool
+import
+(
+	"sync/atomic"
+	"time"
+)
+
+type ThreadPool struct {
+	thread int //线程数
+	maxqueue int //最大队列
+	nreq int32 //一共执行了多少任务
+	jobs chan func()interface{}  //任务集
+	results chan interface{} //返回集
+}
+
+func (this *ThreadPool)worker(id int,){
+	for{
+		j,ok:=<-this.jobs
+		if ok==false{
+			break
+		}
+		this.results <- j()
+	}
+}
+func (this *ThreadPool)Reset(){
+	atomic.StoreInt32(&this.nreq,0)
+	this.jobs=make(chan func()interface{},this.maxqueue)
+	this.results=make(chan interface{},this.maxqueue)
+}
+func (this *ThreadPool)Req(_f func()interface{}){
+	atomic.AddInt32(&this.nreq,1)
+	this.jobs<-_f
+}
+func (this *ThreadPool)Close(){
+	close(this.jobs)
+}
+func (this *ThreadPool)Wait(){
+	for{
+		if atomic.LoadInt32(&this.nreq)==int32(len(this.results)){
+			break
+		}
+		time.Sleep(time.Millisecond*10)
+	}
+}
+func NewThreadPool(_thread int,_maxqueue int)*ThreadPool{
+	pl:=&ThreadPool{thread:_thread}
+	pl.jobs=make(chan func()interface{},_maxqueue)
+	pl.results=make(chan interface{},_maxqueue)
+	for i:=0;i<_thread;i++{
+		go pl.worker(i,)
+	}
+	return pl
+}
